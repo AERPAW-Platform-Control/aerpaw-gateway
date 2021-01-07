80f9c8ea32a1019685628a216a2be891ccd57a43
Erica Fu <ericafu@renci.org>
2021-01-07 17:09:56 -0500

    adduser API

--- swagger_server/controllers/user_controller.py
+++ swagger_server/controllers/user_controller.py
@@ -1,50 +1,25 @@
 import connexion
 import six
 
-from swagger_server.models.api_response import ApiResponse  # noqa: E501
-from swagger_server.models.experiment import Experiment  # noqa: E501
-from swagger_server.models.user import User  # noqa: E501
+from swagger_server.models.userkey import Userkey  # noqa: E501
 from swagger_server import util
 
 
-def create_user(body, username):  # noqa: E501
-    """create user on emulab testbed
+def adduser(body, experiment, project=None):  # noqa: E501
+    """add/update user and sshkey on experiment nodes
 
      # noqa: E501
 
     :param body: User Object
     :type body: dict | bytes
-    :param username: username for the request
-    :type username: str
-
-    :rtype: List[ApiResponse]
-    """
-    if connexion.request.is_json:
-        body = User.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
-
-
-def delete_user(username):  # noqa: E501
-    """delete user
-
-     # noqa: E501
-
-    :param username: username for the request
-    :type username: str
+    :param experiment: experiment for the request
+    :type experiment: str
+    :param project: project of the experiment
+    :type project: str
 
     :rtype: None
     """
+    if connexion.request.is_json:
+        body = Userkey.from_dict(connexion.request.get_json())  # noqa: E501
     return 'do some magic!'
 
-
-def get_user(username):  # noqa: E501
-    """get user information
-
-    get user information # noqa: E501
-
-    :param username: username for the request
-    :type username: str
-
-    :rtype: List[Experiment]
-    """
-    return 'do some magic!'
--- swagger_server/models/__init__.py
+++ swagger_server/models/__init__.py
@@ -9,5 +9,5 @@ from swagger_server.models.node import Node
 from swagger_server.models.profile import Profile
 from swagger_server.models.reservation import Reservation
 from swagger_server.models.resource import Resource
-from swagger_server.models.user import User
+from swagger_server.models.userkey import Userkey
 from swagger_server.models.vnode import Vnode
